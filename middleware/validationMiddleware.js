f58e2df1685a4c33498621878a9e9ca902968c8a
harinder <singhharinder38@gmail.com>
2024-11-22 01:09:01 -0500

    Added Joi validation for user input

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -2,9 +2,12 @@ const Joi = require('joi');
 
 const validateUser = (req, res, next) => {
     const schema = Joi.object({
-        name: Joi.string().min(3).required(),
+        username: Joi.string().min(3).required(),
         email: Joi.string().email().required(),
         password: Joi.string().min(6).required(),
+        address: Joi.string().required(),
+        phone: Joi.string().min(4).required(),
+        answer: Joi.string().required()
     });
 
     const { error } = schema.validate(req.body);
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const { registerController, loginController } = require('../controllers/authController');
 const router = express.Router();
-const validateUser = require('../middlewares/validationMiddleware');
+const validateUser = require('../middleware/validationMiddleware');
 
 //routes
 //Register post
 router.post('/register', validateUser, registerController);
 
 //login Post
-router.post('/login', loginController);
+router.post('/login', validateUser, loginController);
 
 
 
